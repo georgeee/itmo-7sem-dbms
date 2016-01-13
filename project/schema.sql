c6687b137a7cf4ca2ad550e3e108317f9933e225
georgeee <george.agapov@gmail.com>
2016-01-13 16:07:25 +0300

    Implemented schema, constraints, indecies

--- project/schema.sql
+++ project/schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE Venue (
 
 CREATE TABLE City (
   Id SERIAL NOT NULL PRIMARY KEY,
-  CountryId INT NOT NULL,
+  CountryId INT NOT NULL
 );
 
 CREATE TABLE Country (
@@ -57,7 +57,7 @@ CREATE TABLE EventPerformer (
   Instrument EPInstrument
 );
 
-CREATE TABLE User (
+CREATE TABLE PortalUser (
   Id SERIAL NOT NULL PRIMARY KEY,
   Login VARCHAR(50) NOT NULL,
   Name VARCHAR(50)
@@ -78,3 +78,33 @@ CREATE TABLE NotificationEntity (
   EntityType NotificationEntityType NOT NULL,
   EntityId INT NOT NULL
 );
+
+CREATE TABLE Subscription (
+  EntityType NotificationEntityType NOT NULL,
+  EntityId INT NOT NULL,
+  UserId INT NOT NULL
+);
+
+ALTER TABLE Piece ADD CONSTRAINT FK_Piece_ComposerId FOREIGN KEY (ComposerId) REFERENCES Composer (Id);
+ALTER TABLE Piece ADD CONSTRAINT FK_Piece_GeneralPieceId FOREIGN KEY (GeneralPieceId) REFERENCES Piece (Id);
+ALTER TABLE Venue ADD CONSTRAINT FK_Venue_CityId FOREIGN KEY (CityId) REFERENCES City (Id);
+ALTER TABLE City ADD CONSTRAINT FK_City_CountryId FOREIGN KEY (CountryId) REFERENCES Country (Id);
+ALTER TABLE Event ADD CONSTRAINT FK_Event_VenueId FOREIGN KEY (VenueId) REFERENCES Venue (Id);
+ALTER TABLE Event ADD CONSTRAINT FK_Event_PieceId FOREIGN KEY (PieceId) REFERENCES Piece (Id);
+ALTER TABLE EventPerformer ADD CONSTRAINT FK_EventPerformer_EventId FOREIGN KEY (EventId) REFERENCES Event (Id);
+ALTER TABLE EventPerformer ADD CONSTRAINT FK_EventPerformer_PerformerId FOREIGN KEY (PerformerId) REFERENCES Performer (Id);
+ALTER TABLE Subscription ADD CONSTRAINT FK_Subscription_UserId FOREIGN KEY (UserId) REFERENCES PortalUser (Id);
+ALTER TABLE Notification ADD CONSTRAINT FK_Notification_UserId FOREIGN KEY (UserId) REFERENCES PortalUser (Id);
+ALTER TABLE NotificationEntity ADD CONSTRAINT FK_NotificationEntity_NotificationId FOREIGN KEY (NotificationId) REFERENCES Notification (Id);
+
+ALTER TABLE I18n ADD CONSTRAINT UN_I18n UNIQUE (EntityType, EntityId, Key, Lang);
+ALTER TABLE Country ADD CONSTRAINT UN_Country_Code UNIQUE (Code);
+ALTER TABLE PortalUser ADD CONSTRAINT UN_PortalUser_Login UNIQUE (Login);
+ALTER TABLE EventPerformer ADD CONSTRAINT UN_EventPerformer_EventId_PerformerId UNIQUE (EventId, PerformerId);
+ALTER TABLE NotificationEntity ADD CONSTRAINT UN_NotificationEntity UNIQUE (NotificationId, EntityType, EntityId);
+
+CREATE INDEX Event_StartTime ON Event (StartTime);
+CREATE INDEX Event_EndTime ON Event (EndTime);
+
+ALTER TABLE Composer ADD CONSTRAINT Composer_Dates CHECK (DateOfBirth < DateOfDeath);
+ALTER TABLE I18n ADD CONSTRAINT I18n_Key_EntityType CHECK ((Key = 'Name' AND EntityType != 'EventPerformer') OR (Key = 'Address' AND EntityType = 'Venue') OR (Key = 'Role' AND EntityType = 'EventPerformer'));
